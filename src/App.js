a7920662bb480f895d620a051c23ae7ea0c97b1d
xuzhihang@utopa.com.cn <xuzhihang@utopa.com.cn>
2021-09-08 21:07:53 +0800

    修改分片逻辑

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import "./App.css";
 
 let content =
   "不为人知的是，李铁标志性的“吹刘海”动作竟然是在28年前就已经形成。李铁从小性格叛逆，一直想留长发，但是因为在少年队时教练管的太严，不允许队员们头发的刘海长度超过眼睛，李铁被迫只能留着平头。后来李铁跟随健力宝队去巴西训练，教练终于对球员的发型没有太严格的要求，李铁也得以放飞自我，留了一头中分长发，刘海几乎盖住了眼睛。当时的李铁仅有16岁，随着健力宝队在巴西踢球。李铁曾透露当时因为穷，当地理发比较贵，他就留了稍长的头发，刘海挡眼睛了，就会下意识的吹一下，就这样养成了习惯。1993年，引起全国瞩目的健力宝足球队横空出世，张效瑞、商毅、李金羽、郝伟、张永海。。。一个个日后闪耀中国足坛的名字不一而足，李铁也入选了第一批22人最终名单，他留下了一张青涩的照片，那时的他就留着如今这般茂密的刘海。结束===".repeat(
-    50
+    1000
   );
 
 export default function App() {
@@ -80,9 +80,43 @@ export default function App() {
     setFontArr(calFontArr);
     // 处理剩下的数据
     requestIdleCallback(calLimitPage);
+    // calNoLimitPage();
   };
 
   const calLimitPage = (deadline) => {
+    console.log(pageDataArr, fontArr, "first");
+    const windowWidth = document.body.offsetWidth;
+    const fontStyle = "normal 16px Arial";
+    const calAllPage = pageDataArr;
+    let calFontArr =
+      pageDataArr.length > 0 &&
+      content.length > 0 &&
+      pageDataArr[pageDataArr.length - 1].length < textHeight
+        ? pageDataArr.pop()
+        : [];
+    console.log(calFontArr, "cal");
+    while (content.length !== 0 && deadline.timeRemaining() > 0) {
+      const len = charCountFitInWith(windowWidth, content, fontStyle);
+      if (calFontArr.length < textHeight) {
+        calFontArr.push(content.slice(0, len));
+        content = content.slice(len);
+      } else {
+        calAllPage.push(calFontArr);
+        calFontArr = [];
+      }
+    }
+    console.log(calAllPage, "页面已经分好");
+    if (calFontArr.length !== 0) {
+      calAllPage.push(calFontArr);
+      setPageDataArr(calAllPage);
+    }
+    if (content.length > 0) {
+      requestIdleCallback(calLimitPage);
+    }
+  };
+
+  // 无时间分片的
+  const calNoLimitPage = () => {
     console.log(pageDataArr, fontArr, "first");
     const windowWidth = document.body.offsetWidth;
     const fontStyle = "normal 16px Arial";
@@ -93,7 +127,7 @@ export default function App() {
         ? pageDataArr[pageDataArr.length - 1]
         : [];
     // console.log(content);
-    while (content.length !== 0 && deadline.timeRemaining() > 0) {
+    while (content.length !== 0) {
       const len = charCountFitInWith(windowWidth, content, fontStyle);
       if (calFontArr.length < textHeight) {
         calFontArr.push(content.slice(0, len));
@@ -108,9 +142,6 @@ export default function App() {
       calAllPage.push(calFontArr);
     }
     setPageDataArr(calAllPage);
-    if (content.length > 0) {
-      requestIdleCallback(calLimitPage);
-    }
   };
 
   return (
